dd5d51ba937cd6ca1ee0e198ce4ee82486055060
Andre Pautin <andre.pautin@gmail.com>
2021-04-28 12:30:31 -0700

    create user class/table

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
 """Blogly application."""
 
-from flask import Flask
-from models import db, connect_db
+from flask import Flask, request, redirect, render_template
+from flask_debugtoolbar import DebugToolbarExtension
+from models import db, connect_db, User
+
 
 app = Flask(__name__)
+debug = DebugToolbarExtension(app)
+app.config['SECRET_KEY'] = "secret"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 
 connect_db(app)
-db.create_all()
+# db.create_all()
