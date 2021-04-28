96fb90b9f448cd3c05ac517cf337b4ea7ca41603
S T <itschibits@gmail.com>
2021-04-28 17:47:55 -0400

    created some routes and templates

--- app.py
+++ app.py
@@ -6,11 +6,54 @@ from models import db, connect_db, User
 
 
 app = Flask(__name__)
-debug = DebugToolbarExtension(app)
-app.config['SECRET_KEY'] = "secret"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
+app.config['SECRET_KEY'] = "secret"
+debug = DebugToolbarExtension(app)
 
 connect_db(app)
 # db.create_all()
+
+
+@app.route("/")
+def landing_page():
+    return redirect("/users")
+
+
+@app.route("/users")
+def list_users():
+    users = User.query.all()
+    return render_template("users-listing.html", users=users)
+
+
+@app.route("/users/new", methods=["GET", "POST"])
+def show_new_user_form():
+    if (request.method == "POST"):
+        first_name = request.form['first-name']
+        last_name = request.form['last-name']
+        image_url = request.form['image-url']
+
+        user = User(first_name=first_name,
+                    last_name=last_name,
+                    image_url=image_url)
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect("/users")
+    else:
+        return render_template("user-form.html")
+
+
+@app.route("/users/<int:user_id>/edit", methods=["GET", "POST"])
+def edit_user(user_id):
+    user = User.query.get_or_404(user_id)
+    if (request.method == "POST"):
+        user.first_name = request.form['first-name']
+        user.last_name = request.form['last-name']
+        user.image_url = request.form['image-url']
+        db.session.commit()
+        
+        return redirect("/users")
+    else:
+        return render_template("user-edit.html", user=user)
--- models.py
+++ models.py
@@ -13,12 +13,9 @@ class User(db.Model):
 
     __tablename__ = "users"
 
-    id = db.Column(db.Integer, 
-                    primary_key = True, 
-                    autoincrement = True)
-    first_name = db.Column(db.String(20), 
-                            nullable = False)
-    last_name = db.Column(db.String(20), 
-                            nullable = False)
+    id = db.Column(db.Integer,
+                    primary_key=True,
+                    autoincrement=True)
+    first_name = db.Column(db.String(20), nullable=False)
+    last_name = db.Column(db.String(20), nullable=False)
     image_url = db.Column(db.Text)
-
